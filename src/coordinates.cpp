6300a89d3013d3de822498e6f1db35326a1804f9
Jochem Stevense <jochemstevense@protonmail.com>
2022-05-11 11:17:30 +0200

    dev: Added Googletest as testing framework and wrote first unit test

--- src/coordinates.cpp
+++ src/coordinates.cpp
@@ -1,28 +1,12 @@
 #include "coordinates.hpp"
 
-Coordinates::Coordinates(int x_, int y_, int z_): x(x_), y(y_), z(z_){
-}
+Coordinates::Coordinates(int x_, int y_, int z_) : x(x_), y(y_), z(z_) {}
+Coordinates::~Coordinates() {}
 
-Coordinates::~Coordinates(){
+int Coordinates::getX() { return x; }
+int Coordinates::getY() { return y; }
+int Coordinates::getZ() { return z; }
 
-}
-
-int Coordinates::getX(){
-  return x;
-}
-int Coordinates::getY(){
-  return y;
-}
-int Coordinates::getZ(){
-  return z;
-}
-
-void Coordinates::setX(int x_){
-  x=x_;
-}
-void Coordinates::setY(int y_){
-  y=y_;
-}
-void Coordinates::setZ(int z_){
-  z=z_;
-}
+void Coordinates::setX(int x_) { x = x_; }
+void Coordinates::setY(int y_) { y = y_; }
+void Coordinates::setZ(int z_) { z = z_; }
