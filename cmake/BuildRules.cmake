319da81f8583038503c99461baa78fb69c5d3bd1
jochemste <jochemstevense@protonmail.com>
2023-11-22 21:29:54 +0100

    Cleaned cmakelists and added buildrule cmake file

--- cmake/BuildRules.cmake
+++ cmake/BuildRules.cmake
@@ -5,3 +5,5 @@ set(CMAKE_CXX_FLAGS "-Wall -Wextra")
 
 # Below line is for using C++20 standard
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++20")
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${${PROJECTNAME}_SOURCE_DIR}/bin)
