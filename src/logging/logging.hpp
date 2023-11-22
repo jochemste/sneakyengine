2d462c6781df316ddc6e2d289627fc8219d09260
jochemste <jochemstevense@protonmail.com>
2023-11-22 21:36:42 +0100

    Added logging lib

---
+++ src/logging/logging.hpp
@@ -0,0 +1,45 @@
+#ifndef LOGGING_HPP
+#define LOGGING_HPP
+
+#include "ilog_instance.hpp"
+
+#include <iostream>
+#include <memory>
+#include <sstream>
+
+#define LOG_HEADER \
+  __FILE__ << ":" << __builtin_FUNCTION() << ":" << __LINE__ << " >> "
+#define LOG_START LOG_HEADER << "Start function"
+#define LOG_END   LOG_HEADER << "End function"
+
+enum class LogLevel { critical, error, warning, info, debug };
+
+/// @brief Main logging
+class Log {
+public:
+  Log(LogLevel log_level = LogLevel::error);
+  Log(std::unique_ptr<ILogInstance> logger,
+      LogLevel log_level = LogLevel::error);
+  ~Log();
+
+  /// @brief Flush the buffer
+  void flush();
+
+  void set_loglevel(LogLevel log_level);
+
+  /// @brief Logging to be used as a stream
+  template <typename T> Log &operator<<(T const &value) {
+    m_buffer << value;
+    return *this;
+  }
+
+protected:
+private:
+  /// Logging level to use for the logger implementation
+  static LogLevel m_loglevel_app;
+  LogLevel m_loglevel_buffer;
+  std::ostringstream m_buffer;
+  std::unique_ptr<ILogInstance> m_logger;
+};
+
+#endif // LOGGING_HPP
