3bca7eb790dabff00a78f77bb5ed6e97bd27defc
jochemste <jochemstevense@protonmail.com>
2023-11-22 21:36:53 +0100

    Added logging to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include "engine.hpp"
 
+#include <logging.hpp>
+
 #include <iostream>
 
-int main(int argc, char **argv) {
-  std::cout << "Starting test application" << std::endl;
+int main(int /*argc*/, char ** /*argv*/) {
+  Log().set_loglevel(LogLevel::debug);
+  Log(LogLevel::debug) << LOG_START;
+  Log(LogLevel::debug) << LOG_HEADER << "Starting test application";
 
   Engine engine;
   int res = engine.run();
-
   if (res) {
     std::cout << "An error occurred. Error code: " << res << std::endl;
   }
+  engine.stop();
 }
