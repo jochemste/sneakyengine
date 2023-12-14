9411ea25fc6adad676e1ad92c6042bdd3300f6b5
jochemste <jochemstevense@protonmail.com>
2023-12-14 20:26:09 +0100

    Test for opening and closing window

--- src/display/display_sdl_impl.cpp
+++ src/display/display_sdl_impl.cpp
@@ -3,7 +3,17 @@
 
 void DisplaySDLImpl::start() {
   Log(LogLevel::info) << LOG_HEADER << "Starting display";
+  m_window = SDL_CreateWindow("SneakyEngine", 680, 480, SDL_WINDOW_OPENGL);
+
+  if (m_window == nullptr) {
+    Log(LogLevel::critical) << "Failed to create window:" << SDL_GetError();
+  }
 }
 void DisplaySDLImpl::stop() {
   Log(LogLevel::info) << LOG_HEADER << "Stopping display";
+
+  // Temporary debug delay
+  SDL_Delay(1000);
+
+  SDL_DestroyWindow(m_window);
 }
--- src/display/display_sdl_impl.hpp
+++ src/display/display_sdl_impl.hpp
@@ -1,7 +1,12 @@
 #include "display.hpp"
 
+#include "SDL3/SDL.h"
+
 class DisplaySDLImpl : public IDisplay {
 public:
   virtual void start() override;
   virtual void stop() override;
+
+private:
+  SDL_Window *m_window;
 };
