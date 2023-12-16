7c6bf3dc5cc2d74370307ca2907578201219d4c1
jochemste <jochemstevense@protonmail.com>
2023-12-16 16:58:44 +0100

    Added error handling and FPS support
    Added exception throwing, window surface, and frame rate based refreshing

--- src/display/display_sdl_impl.cpp
+++ src/display/display_sdl_impl.cpp
@@ -1,19 +1,63 @@
 #include "display_sdl_impl.hpp"
+
 #include "logging.hpp"
+#include "time.hpp"
+
+// Temporary constexpr. Data will be retrieved differently in future iteration
+constexpr const char *g_window_name = "SneakyEngine";
+constexpr int g_window_width        = 680;
+constexpr int g_window_height       = 480;
+constexpr Uint32 g_window_flags     = SDL_WINDOW_OPENGL;
+constexpr int g_window_fps          = 60;
+
+DisplaySDLImpl::DisplaySDLImpl()
+    : m_window(nullptr), m_surface(nullptr),
+      m_tick_interval(utils::time::nsecond / g_window_fps) {}
+DisplaySDLImpl::~DisplaySDLImpl() {}
 
 void DisplaySDLImpl::start() {
   Log(LogLevel::info) << LOG_HEADER << "Starting display";
-  m_window = SDL_CreateWindow("SneakyEngine", 680, 480, SDL_WINDOW_OPENGL);
 
+  m_window = SDL_CreateWindow(g_window_name, g_window_width, g_window_height,
+                              g_window_flags);
   if (m_window == nullptr) {
-    Log(LogLevel::critical) << "Failed to create window:" << SDL_GetError();
+    const std::string err_msg("Failed to create window");
+    Log(LogLevel::critical) << err_msg << ": " << SDL_GetError();
+    throw DisplayException(err_msg);
+  } else {
+    m_surface = SDL_GetWindowSurface(m_window);
   }
+
+  if (m_surface == nullptr) {
+    const std::string err_msg("Failed to get surface from window");
+    Log(LogLevel::critical) << err_msg << ": " << SDL_GetError();
+    throw DisplayException(err_msg);
+  }
+
+  m_next_frame_tick_nr = SDL_GetTicks() + m_tick_interval;
 }
 void DisplaySDLImpl::stop() {
   Log(LogLevel::info) << LOG_HEADER << "Stopping display";
 
-  // Temporary debug delay
-  SDL_Delay(1000);
+  if (SDL_DestroyWindowSurface(m_window) != 0) {
+    const std::string err_msg("Failed to destroy window surface");
+    Log(LogLevel::error) << err_msg;
+    throw DisplayException(err_msg);
+  }
 
   SDL_DestroyWindow(m_window);
+}
+
+void DisplaySDLImpl::refresh() {
+  SDL_UpdateWindowSurface(m_window);
+  this->waitFPS();
+}
+
+// PRIVATE
+void DisplaySDLImpl::waitFPS() {
+  auto current_tick = SDL_GetTicks();
+  if (current_tick < m_next_frame_tick_nr) {
+    SDL_Delay(m_next_frame_tick_nr - current_tick);
+  }
+  m_next_frame_tick_nr += m_tick_interval;
 }
--- src/display/display_sdl_impl.hpp
+++ src/display/display_sdl_impl.hpp
@@ -4,9 +4,21 @@
 
 class DisplaySDLImpl : public IDisplay {
 public:
+  DisplaySDLImpl();
+  ~DisplaySDLImpl();
+
   virtual void start() override;
   virtual void stop() override;
+  virtual void refresh() override;
 
 private:
+  /// @brief Wait to match framerate
+  void waitFPS();
+
   SDL_Window *m_window;
+  SDL_Surface *m_surface;
+
+  const Uint32 m_tick_interval;
+
+  Uint32 m_next_frame_tick_nr;
 };
