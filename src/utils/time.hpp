f5b050595561e207520b7bdef0ccd105b2848da2
jochemste <jochemstevense@protonmail.com>
2023-12-16 16:56:01 +0100

    Added new utils module for generic definitions and helpers

---
+++ src/utils/time.hpp
@@ -0,0 +1,12 @@
+/// Utility definitions
+namespace utils {
+/// Time related
+namespace time {
+constexpr int nsecond = 1000;
+constexpr int nminute = nsecond * 60;
+constexpr int nhour   = nminute * 60;
+constexpr int fsecond = 1000.0;
+constexpr int fminute = fsecond * 60.0;
+constexpr int fhour   = fminute * 60.0;
+} // namespace time
+} // namespace utils
