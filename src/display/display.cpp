fea565021f9e68dff038029e866a4d7669a977d8
jochemste <jochemstevense@protonmail.com>
2023-12-16 16:57:22 +0100

    Added custom exception and refresh to display interface

--- src/display/display.cpp
+++ src/display/display.cpp
@@ -4,3 +4,10 @@
 std::unique_ptr<IDisplay> DIS_get_display_instance() {
   return std::make_unique<DisplaySDLImpl>();
 }
+
+DisplayException::DisplayException(const char *message) : m_message(message) {}
+
+DisplayException::DisplayException(const std::string &message)
+    : DisplayException(message.c_str()) {}
+
+const char *DisplayException::what() { return m_message; }
--- src/display/display.hpp
+++ src/display/display.hpp
@@ -3,13 +3,31 @@
 
 #include <memory>
 
+/// Interface to display
 class IDisplay {
 public:
+  /// @brief Start the display
   virtual void start() = 0;
-  virtual void stop()  = 0;
+
+  /// @brief Stop the display
+  virtual void stop() = 0;
+
+  /// @brief Refresh the diplay
+  virtual void refresh() = 0;
 };
 
 /// @brief Factory-ish function to get display implementation
 std::unique_ptr<IDisplay> DIS_get_display_instance();
+
+/// @brief General display exception class
+class DisplayException : public std::exception {
+public:
+  DisplayException(const char *message);
+  DisplayException(const std::string &message);
+  const char *what();
+
+private:
+  const char *m_message;
+};
 
 #endif // DISPLAY_HPP
