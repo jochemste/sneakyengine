c1d1427ee7a9ff395c3af3815d41d5b93758b2bd
jochemste <jochemstevense@protonmail.com>
2023-12-21 19:10:09 +0100

    Implemented display process

--- src/process/process_impl/display_proc_impl.cpp
+++ src/process/process_impl/display_proc_impl.cpp
@@ -1,9 +1,11 @@
 #include "display_proc_impl.hpp"
 
 #include "display.hpp"
+#include "logging.hpp"
 #include "process.hpp"
 
 #include <memory>
+#include <mutex>
 
 class DisplayProcessImpl : public IProcess {
 public:
@@ -17,10 +19,14 @@ public:
   virtual ProcessOwner get_owner() override;
 
 private:
+  void update_state(const ProcessState &state);
+
   ProcessOwner m_owner;
   int m_process_id;
   std::string m_name;
   ProcessState m_state;
+  std::unique_ptr<IDisplay> m_display;
+  std::mutex m_state_mutex;
 };
 
 std::unique_ptr<IProcess>
@@ -32,9 +38,60 @@ DisplayProcessFactory::createProcess(const ProcessOwner &owner,
 DisplayProcessImpl::DisplayProcessImpl(const ProcessOwner &owner,
                                        const std::string &name)
     : m_owner(owner), m_process_id(-1), m_name(name),
-      m_state(ProcessState::not_running) {}
-DisplayProcessImpl::~DisplayProcessImpl() {}
-void DisplayProcessImpl::execute(int id) { m_process_id = id; }
-void DisplayProcessImpl::kill() { m_process_id = -1; }
-ProcessState DisplayProcessImpl::get_state() { return m_state; }
+      m_state(ProcessState::not_running),
+      m_display(DIS_get_display_instance()) {}
+
+DisplayProcessImpl::~DisplayProcessImpl() { kill(); }
+
+void DisplayProcessImpl::execute(int id) {
+  Log(LogLevel::info) << LOG_HEADER << "Executing process" << id << ":"
+                      << m_name;
+  m_process_id = id;
+  update_state(ProcessState::running);
+
+  try {
+    m_display->start();
+
+    while (get_state() == ProcessState::running) {
+      m_display->refresh();
+    }
+
+  } catch (DisplayException &e) {
+    update_state(ProcessState::failed);
+    std::string err_msg("Display process has failed");
+    Log(LogLevel::critical) << LOG_HEADER << err_msg << ": " << e.what();
+    throw ProcessException(err_msg);
+  }
+}
+
+void DisplayProcessImpl::kill() {
+  if (get_state() == ProcessState::running) {
+    update_state(ProcessState::stopping);
+
+    try {
+      m_display->stop();
+    } catch (DisplayException &e) {
+      update_state(ProcessState::failed);
+      std::string err_msg("Killing display has failed");
+      Log(LogLevel::error) << LOG_HEADER << err_msg << ": " << e.what();
+      throw ProcessException(err_msg);
+    }
+  }
+
+  if (get_state() != ProcessState::failed) {
+    update_state(ProcessState::finished);
+  }
+  m_process_id = -1;
+}
+
+ProcessState DisplayProcessImpl::get_state() {
+  const std::lock_guard<std::mutex> lock(m_state_mutex);
+  return m_state;
+}
+
 ProcessOwner DisplayProcessImpl::get_owner() { return m_owner; }
+
+void DisplayProcessImpl::update_state(const ProcessState &state) {
+  const std::lock_guard<std::mutex> lock(m_state_mutex);
+  m_state = state;
+}
