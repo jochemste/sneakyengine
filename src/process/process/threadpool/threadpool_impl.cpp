158b4e908471a3b1be25faef9256e64ccedbfd44
jochemste <jochemstevense@protonmail.com>
2024-01-30 16:39:01 +0100

    First implementation of threadpool

--- src/process/process/threadpool/threadpool_impl.cpp
+++ src/process/process/threadpool/threadpool_impl.cpp
@@ -1,8 +1,90 @@
 #include "threadpool_impl.hpp"
 
+#include "logging.hpp"
+
 namespace threadpool {
-Threadpool_impl::Threadpool_impl(int nr_processes) {}
-void Threadpool_impl::start() {}
-void Threadpool_impl::stop() {}
-void Threadpool_impl::add_to_queue(std::shared_ptr<IProcess> process) {}
+Threadpool_impl::Threadpool_impl()
+    : Threadpool_impl(std::thread::hardware_concurrency()) {
+  Log(LogLevel::debug) << LOG_START;
+  Log(LogLevel::debug) << LOG_END;
+}
+
+Threadpool_impl::Threadpool_impl(int nr_threads)
+    : m_nr_threads(nr_threads), m_should_stop(false) {
+  Log(LogLevel::debug) << LOG_START;
+
+  Log(LogLevel::debug) << LOG_END;
+}
+
+void Threadpool_impl::start() {
+  Log(LogLevel::debug) << LOG_START;
+
+  for (int i = 0; i < m_nr_threads; i++) {
+    m_threads.emplace_back(std::thread(&Threadpool_impl::thread_run, this));
+  }
+
+  Log(LogLevel::debug) << LOG_END;
+}
+
+void Threadpool_impl::stop() {
+  Log(LogLevel::debug) << LOG_START;
+
+  {
+    std::unique_lock<std::mutex> lock(m_queue_mutex);
+    m_should_stop = true;
+  }
+  m_mutex_condition.notify_all();
+
+  for (auto &thread : m_threads) {
+    thread.join();
+  }
+  m_threads.clear();
+
+  Log(LogLevel::debug) << LOG_END;
+}
+
+void Threadpool_impl::add_to_queue(const int &id,
+                                   std::shared_ptr<IProcess> process) {
+  Log(LogLevel::debug) << LOG_START;
+  {
+    std::unique_lock<std::mutex> lock(m_queue_mutex);
+    m_queue.push(std::make_pair(id, process));
+  }
+  m_mutex_condition.notify_one();
+
+  Log(LogLevel::debug) << LOG_END;
+}
+
+void Threadpool_impl::thread_run() {
+  Log(LogLevel::debug) << LOG_START;
+
+  while (true) {
+    int id = -1;
+    std::shared_ptr<IProcess> process;
+
+    {
+      std::unique_lock<std::mutex> lock(m_queue_mutex);
+      m_mutex_condition.wait(
+          lock, [this] { return !m_queue.empty() || m_should_stop; });
+      if (m_should_stop) {
+        return;
+      }
+
+      Log(LogLevel::debug) << LOG_HEADER << "Getting id and process to execute";
+      id      = m_queue.front().first;
+      process = m_queue.front().second;
+      m_queue.pop();
+    }
+
+    if ((process != nullptr)) {
+      Log(LogLevel::debug) << LOG_HEADER << "Executing process with id " << id;
+      process->execute(id);
+    } else {
+      Log(LogLevel::error) << LOG_HEADER << "Could not start process with id "
+                           << id << ". Process is null or id is invalid";
+    }
+  }
+
+  Log(LogLevel::debug) << LOG_END;
+}
 } // namespace threadpool
--- src/process/process/threadpool/threadpool_impl.hpp
+++ src/process/process/threadpool/threadpool_impl.hpp
@@ -3,13 +3,34 @@
 
 #include "threadpool.hpp"
 
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <thread>
+
 namespace threadpool {
 class Threadpool_impl : public IThreadpool {
 public:
-  Threadpool_impl(int nr_processes);
+  Threadpool_impl();
+  Threadpool_impl(int nr_threads);
+  Threadpool_impl(const Threadpool_impl &other) = delete;
+  Threadpool_impl(Threadpool_impl &&other)      = default;
+
+  ~Threadpool_impl() {}
   virtual void start() override;
   virtual void stop() override;
-  virtual void add_to_queue(std::shared_ptr<IProcess> process) override;
+  virtual void add_to_queue(const int &id,
+                            std::shared_ptr<IProcess> process) override;
+
+private:
+  void thread_run();
+
+  const int m_nr_threads;
+  bool m_should_stop;
+  std::mutex m_queue_mutex;
+  std::vector<std::thread> m_threads;
+  std::condition_variable m_mutex_condition;
+  std::queue<std::pair<int, std::shared_ptr<IProcess>>> m_queue;
 };
 } // namespace threadpool
 
