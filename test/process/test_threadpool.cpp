763c9c981eeb447d0cc1988059beb54c6c25238f
jochemste <jochemstevense@protonmail.com>
2024-01-30 21:09:48 +0100

    Replaced sleeps with state checking

--- test/process/test_threadpool.cpp
+++ test/process/test_threadpool.cpp
@@ -6,29 +6,31 @@
 
 #include "mock_process.hpp"
 
-#include <chrono>
-#include <thread>
-
+using ::testing::_;
+using ::testing::AtLeast;
 using ::testing::Invoke;
+using ::testing::Throw;
 
 class TestThreadpool : public ::testing::Test {};
 
+// Test a single threaded pool without processes to run without throwing or
+// segfaulting
 TEST_F(TestThreadpool, TestThreadpoolSingleThreadNoProcess) {
   threadpool::Threadpool_impl tp = threadpool::Threadpool_impl(1);
 
   EXPECT_NO_THROW({
     tp.start();
-    std::this_thread::sleep_for(std::chrono::nanoseconds(10));
     tp.stop();
   });
 }
 
+// Test a multi threaded pool without processes to run without throwing or
+// segfaulting
 TEST_F(TestThreadpool, TestThreadpoolSysNrThreadNoProcess) {
   threadpool::Threadpool_impl tp = threadpool::Threadpool_impl();
 
   EXPECT_NO_THROW({
     tp.start();
-    std::this_thread::sleep_for(std::chrono::nanoseconds(10));
     tp.stop();
   });
 }
@@ -44,7 +46,8 @@ TEST_F(TestThreadpool, TestThreadpoolSysNrThreadOneProcess) {
   EXPECT_NO_THROW({
     tp.start();
     tp.add_to_queue(id, process);
-    std::this_thread::sleep_for(std::chrono::nanoseconds(10));
+    while (tp.busy())
+      ;
     tp.stop();
   });
 }
@@ -53,17 +56,39 @@ TEST_F(TestThreadpool, TestThreadpoolSysNrThreadMultiProcess) {
   std::shared_ptr<MockProcess> process(
       new MockProcess(ProcessOwner::process_manager, "testProcess1"));
   threadpool::Threadpool_impl tp = threadpool::Threadpool_impl();
-  auto f                         = [](int id) {
-    std::cout << "Testing " << id << std::endl;
-    std::this_thread::sleep_for(std::chrono::nanoseconds(10));
-  };
+  auto f = [](int id) { std::cout << "Testing " << id << std::endl; };
+
+  for (int id = 0; id < 100; id++) {
+    EXPECT_CALL(*process.get(), execute(id)).Times(1).WillOnce(Invoke(f));
+  }
+
+  EXPECT_NO_THROW({
+    tp.start();
+    for (int id = 0; id < 100; id++) {
+      tp.add_to_queue(id, process);
+    }
+
+    while (tp.busy())
+      ;
+    tp.stop();
+  });
+
+  EXPECT_EQ(0, tp.get_nr_running());
+  EXPECT_EQ(0, tp.get_nr_queued());
+}
+
+TEST_F(TestThreadpool, TestThreadpoolSysNrThreadMultiProcessThrows) {
+  std::shared_ptr<MockProcess> process(
+      new MockProcess(ProcessOwner::process_manager, "testProcess1"));
+  threadpool::Threadpool_impl tp = threadpool::Threadpool_impl();
+  auto f = [](int id) { std::cout << "Testing " << id << std::endl; };
 
   {
-    for (int id = 0; id < 100; id++) {
-      EXPECT_CALL(*process.get(), execute(id))
-          .Times(1)
-          .WillRepeatedly(Invoke(f));
-    }
+    testing::InSequence s;
+    EXPECT_CALL(*process.get(), execute(_)).Times(99).WillRepeatedly(Invoke(f));
+    EXPECT_CALL(*process.get(), execute(_))
+        .Times(1)
+        .WillOnce(Throw(std::exception()));
   }
 
   EXPECT_NO_THROW({
