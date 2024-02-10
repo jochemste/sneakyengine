2f54678170181decde0a9735b0dadf911f938480
jochemste <jochemstevense@protonmail.com>
2024-02-10 12:08:39 +0100

    Added more warnings to cxx flags

--- cmake/BuildRules.cmake
+++ cmake/BuildRules.cmake
@@ -1,9 +1,20 @@
 message(STATUS "Setting buildrules")
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_FLAGS "-g -Wall -Wextra")
+
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+  set(CMAKE_CXX_FLAGS "-g")
+endif()
+
+# warnings
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wconversion -Wno-sign-conversion -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function")
+
+# sanitize
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address,undefined")
 
 # Below line is for using C++20 standard
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++20")
+
+message("Compiling with flags: ${CMAKE_CXX_FLAGS}")
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${${PROJECTNAME}_SOURCE_DIR}/bin)
