f5447e9520019794f3ee992a60be315a26a33aea
jochemste <jochemstevense@protonmail.com>
2024-02-10 15:55:02 +0100

    Disabled sanitize

--- cmake/BuildRules.cmake
+++ cmake/BuildRules.cmake
@@ -10,7 +10,7 @@ endif()
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wconversion -Wno-sign-conversion -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function")
 
 # sanitize
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address,undefined")
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address,undefined")
 
 # Below line is for using C++20 standard
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++20")
