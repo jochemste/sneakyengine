8ad6ca4c174e84b490716b2267d8722f6eac69db
jochemste <jochemstevense@protonmail.com>
2024-03-17 17:42:40 +0100

    Added conditionals to allow compilation for non GCC compilers

--- cmake/BuildRules.cmake
+++ cmake/BuildRules.cmake
@@ -2,18 +2,26 @@ message(STATUS "Setting buildrules")
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-  set(CMAKE_CXX_FLAGS "-g")
-endif()
+# GCC builds
+if(CMAKE_COMPILER_IS_GNUCXX)
+  message("GCC compiler found.")
+  if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(CMAKE_CXX_FLAGS "-g")
+  endif()
 
-# warnings
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wconversion -Wno-sign-conversion -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function")
+  # warnings
+  set(CMAKE_CXX_FLAGS
+    "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wconversion -Wno-sign-conversion -Wdouble-promotion -Wno-unused-parameter -Wno-unused-function")
 
-# sanitize
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address,undefined")
+  # sanitize
+  #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address,undefined")
 
-# Below line is for using C++20 standard
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++20")
+  # Below line is for using C++20 standard
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++20")
+else()
+  message("WARNING: GCC not found, ${PROJECTNAME} build untested")
+endif(CMAKE_COMPILER_IS_GNUCXX)
+# End of GCC builds
 
 message("Compiling with flags: ${CMAKE_CXX_FLAGS}")
 
