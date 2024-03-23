6a34be449929c93724b34a61ead626647429fc41
jochemste <jochemstevense@protonmail.com>
2024-03-23 12:06:29 +0100

    Added types utility header

---
+++ src/utils/types.hpp
@@ -0,0 +1,10 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+namespace utils {
+namespace types {
+enum type { e_INT = 0, e_FLOAT, e_DOUBLE, e_STRING, e_ };
+}
+} // namespace utils
+
+#endif // TYPES_HPP
