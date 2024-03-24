8906df07faee8ed8de6c988fa7b683843b9be178
jochemste <jochemstevense@protonmail.com>
2024-03-24 18:06:13 +0100

    Continued implementation of message

--- src/common/mod_server/impl/message/default/message_impl.hpp
+++ src/common/mod_server/impl/message/default/message_impl.hpp
@@ -2,6 +2,7 @@
 
 #include "types.hpp"
 
+#include <functional>
 #include <string>
 
 namespace common {
@@ -17,25 +18,27 @@ public:
   ~MessageImpl();
 
   virtual const std::string get_subject() override;
-  virtual int get_subject_id() override;
+  virtual unsigned long int get_subject_id() override;
   virtual int get_message_id() override;
   virtual bool get_message(std::string &message_str) override;
   virtual bool get_message(int &message_int) override;
   virtual utils::types::type get_type() override;
 
   virtual void set_subject(const std::string &subject) override;
-  virtual void set_subject_id(const int &id) override;
   virtual void set_message(const std::string &message_str) override;
   virtual void set_message(const int &message_int) override;
 
 private:
+  void set_subject_id(const unsigned long int &id);
+
   std::string m_subject;
   std::string m_message_str;
   int m_message_int;
-  int m_subject_id;
+  unsigned long int m_subject_id;
   utils::types::type m_type = utils::types::e_STRING;
   bool m_subject_set;
   bool m_message_set;
+  std::hash<std::string> m_subject_hasher;
 
   static int m_message_id;
 };
