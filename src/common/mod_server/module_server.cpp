c847a27614bd1052ca93425707bbad4774b3677b
jochemste <jochemstevense@protonmail.com>
2024-04-02 20:46:57 +0200

    Added factory implementation

--- src/common/mod_server/module_server.cpp
+++ src/common/mod_server/module_server.cpp
@@ -1,5 +1,11 @@
 #include "server.hpp"
 
+#include "message_impl.hpp"
+
 namespace common {
-namespace mod_server {}
+namespace mod_server {
+std::shared_ptr<IMessage> MessageFactory::create() {
+  return std::make_unique<MessageImpl>();
+}
+} // namespace mod_server
 } // namespace common
