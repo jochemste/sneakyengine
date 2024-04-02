7a85e1fa5247f27082cec55f35b535cabc72bbba
jochemste <jochemstevense@protonmail.com>
2024-04-02 20:47:52 +0200

    Added tests for IMessage and MessageImpl

--- test/mod_server/test_message.cpp
+++ test/mod_server/test_message.cpp
@@ -2,6 +2,8 @@
 
 #include "message_impl.hpp"
 
+#include "types.hpp"
+
 #include <functional>
 #include <string>
 
@@ -15,8 +17,16 @@ protected:
 
   const std::string m_subject     = "Some subject";
   const std::string m_message_str = "Some message string";
+  const int m_message_int         = 42;
 
 private:
+};
+
+class TestMessageInterface : public ::testing::Test {
+protected:
+  const std::string m_subject     = "Some subject";
+  const std::string m_message_str = "Some message string";
+  const int m_message_int         = 42;
 };
 
 namespace srv = common::mod_server;
@@ -52,4 +62,72 @@ TEST_F(TestMessageImplDefault, TestGetSubjectID) {
   srv::MessageImpl msg(m_subject, m_message_str);
 
   EXPECT_EQ(get_hashed_subj(), msg.get_subject_id());
+}
+
+/// Test if get_message() retrieved message string and errors at trying to
+/// retrieve message int
+TEST_F(TestMessageImplDefault, TestGetMessageStr) {
+  srv::MessageImpl msg(m_subject, m_message_str);
+  std::string message_str;
+  int message_int;
+
+  EXPECT_TRUE(msg.get_message(message_str));
+  EXPECT_FALSE(msg.get_message(message_int));
+
+  EXPECT_EQ(m_message_str, message_str);
+}
+
+/// Test if get_message() retrieved message int and errors at trying to
+/// retrieve message string
+TEST_F(TestMessageImplDefault, TestGetMessageInt) {
+  srv::MessageImpl msg(m_subject);
+  std::string message_str;
+  int message_int;
+
+  msg.set_message(m_message_int);
+
+  EXPECT_TRUE(msg.get_message(message_int));
+  EXPECT_FALSE(msg.get_message(message_str));
+
+  EXPECT_EQ(m_message_int, message_int);
+}
+
+/// Test the same as the other tests, but then through the external interface,
+/// by calling the factory class to get a IMessage instance
+TEST_F(TestMessageInterface, TestFullInterfaceStr) {
+  using utils::types::e_STRING;
+  auto imsg           = srv::MessageFactory().create();
+  std::string msg_str = "";
+
+  ASSERT_NO_THROW({
+    imsg->set_subject(m_subject);
+    imsg->set_message(m_message_str);
+    imsg->get_message(msg_str);
+  });
+
+  EXPECT_EQ(m_subject, imsg->get_subject());
+  EXPECT_EQ(m_message_str, msg_str);
+  EXPECT_NE(imsg->get_message_id(), 0);
+  EXPECT_EQ(imsg->get_type(), e_STRING);
+  EXPECT_NE(imsg->get_subject_id(), 0);
+}
+
+/// Test the same as the other tests, but then through the external interface,
+/// by calling the factory class to get a IMessage instance
+TEST_F(TestMessageInterface, TestFullInterfaceInt) {
+  using utils::types::e_INT;
+  auto imsg   = srv::MessageFactory().create();
+  int msg_int = 0;
+
+  ASSERT_NO_THROW({
+    imsg->set_subject(m_subject);
+    imsg->set_message(m_message_int);
+    imsg->get_message(msg_int);
+  });
+
+  EXPECT_EQ(m_subject, imsg->get_subject());
+  EXPECT_EQ(m_message_int, msg_int);
+  EXPECT_NE(imsg->get_message_id(), 0);
+  EXPECT_EQ(imsg->get_type(), e_INT);
+  EXPECT_NE(imsg->get_subject_id(), 0);
 }
